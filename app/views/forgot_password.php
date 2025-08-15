4cda2d869f9fa6aa2e729f0245bd2e2c2a92068d
epywiki <ludwigspeke@gmail.com>
2025-08-15 07:30:47 +0300

    Update forgot_password.php

--- app/views/forgot_password.php
+++ app/views/forgot_password.php
@@ -19,14 +19,5 @@ include 'header.php'; ?>
     </div>
 </div>
 
-<script src="https://unpkg.com/tai-password-strength@1.1.3/lib/password-strength.js"></script>
-<script>
-  const passwordInput = document.querySelector('input[name="req_password"]');
-  const strengthIndicator = document.createElement('div');
-  passwordInput.parentNode.appendChild(strengthIndicator);
 
-  passwordInput.addEventListener('input', () => {
-      const result = taiPasswordStrength(passwordInput.value);
-      strengthIndicator.textContent = 'Strength: ' + result.strength;
-  });
-</script>
+
