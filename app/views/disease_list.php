22aaa87ba1807b565de7a23ecd3aab61557f0f31
epywiki <ludwigspeke@gmail.com>
2025-08-15 07:29:53 +0300

    Update disease_list.php

--- app/views/disease_list.php
+++ app/views/disease_list.php
@@ -7,10 +7,11 @@ include 'header.php';
 <?php foreach ($diseases as $disease): ?>
     <li>
         <?= htmlspecialchars($disease['name']) ?>
-        - <a href="index.php?page=edit_disease&id=<?= $disease['id'] ?>">Edit</a>
-        - <a href="index.php?page=delete_disease&id=<?= $disease['id'] ?>" 
+        - <a href="?page=edit_disease&id=<?= $disease['id'] ?>">Edit</a>
+        - <a href="?page=delete_disease&id=<?= $disease['id'] ?>" 
              onclick="return confirm('Are you sure?')">Delete</a>
     </li>
 <?php endforeach; ?>
 </ul>
+
 
